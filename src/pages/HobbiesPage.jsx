a5300b8a7e69771c23787aa2d781b8c9ab5f7822
broBbang2 <msk990719@gmail.com>
2025-11-24 23:16:12 +0900

    [feat]오늘의 습관 구조

---
+++ src/pages/HobbiesPage.jsx
@@ -0,0 +1,28 @@
+import Header from "../components/Header";
+import { Title } from "../mock/Title";
+import { Date } from "../utils/Date";
+import { Chip } from "../components/Atoms/Chip";
+
+const HobbiesPasge = () => {
+  return (
+    <>
+      <Header />
+      <div className="g_box">
+        <h3>{Title}</h3>
+        <button>오늘의 집중</button>
+        <button>홈</button>
+        <div className="TodayTime">
+          <h3>현재시간</h3>
+          <Date />
+        </div>
+        <article className="contatiner">
+          <h3>오늘의 습관</h3>
+          <button>목록 수정</button>
+          <Chip /> {/*여기서 이제 여러개의 chip이 들어감 */}
+        </article>
+      </div>
+    </>
+  );
+};
+
+export default HobbiesPasge;
