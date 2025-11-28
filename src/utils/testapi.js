78b3d4f1c396cea5b87dfe4ddba748ff85fb43af
dhf2011 <dldlsckd133@gmail.com>
2025-11-28 14:44:51 +0900

    [feat] 이모지 api연동 완료

--- src/utils/testapi.js
+++ src/utils/testapi.js
@@ -1,5 +1,49 @@
-export default async function getStudyList(id) {
-  const response = await fetch(`http://localhost:4000/api/studies/${id}`);
+const BASE_URL = 'http://172.30.1.30:4000/api/';
+
+export async function getStudyList(id) {
+  const response = await fetch(`${BASE_URL}studies/${id}`);
+  const body = await response.json();
+  return body;
+}
+
+// export async function postStudyListEmoji(emojiformData) {
+//   const response = await fetch(`http://172.30.1.30:4000/api/studies/3/emojis`, {
+//     method: 'POST',
+
+//     body: emojiformData,
+//   });
+//   const body = await response.json();
+//   return body;
+// }
+
+// 이모지 추가하는 api
+
+export async function postStudyListEmoji(id, emojiCode) {
+  try {
+    const response = await fetch(`${BASE_URL}studies/${id}/emojis`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ emojiCode }), // JSON 형식으로 보내기
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error('Error posting emoji:', error);
+    return null;
+  }
+}
+
+// 이모지 조회하는 api
+
+export async function getStudyListEmoji(id) {
+  const response = await fetch(`${BASE_URL}studies/${id}/emojis`);
   const body = await response.json();
   return body;
 }
